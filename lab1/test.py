514de554dc179c3b4fe93c2c2603dd2f895af1f5
Pawel Konopka <p.konopka@avsystem.com>
2023-10-17 00:18:36 +0200

    lab1 finished

---
+++ lab1/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'San Francisco', 'Los Angeles']
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+filtered_df = df['Age'] >= 30
+print(filtered_df)
